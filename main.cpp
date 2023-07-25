2db8fad38a067e8bfee719dd12cdcd34ea48718c
seanwascoding <a225108347171@gmail.com>
2023-07-25 17:58:36 +0800

    以解決chunk問題 => final version

--- main.cpp
+++ main.cpp
@@ -6,26 +6,31 @@ int main()
     //!
     sha hash;
     string temp;
+    cout << "input the message:";
     getline(cin, temp);
     vector<bitset<8>> test = hash.message_transfer(temp);
-    // cout << "test:" << test[0] << endl;
+    // cout << "test:" << test.size() << endl;
 
     //! output initialize state
     // hash.state();
     // cout << endl;
 
-    //! step 2
-    vector<bitset<32>> test2 = hash.chunk(test);
-    // for (int i = 0; i < test2.size(); i++)
-    // {
-    //     cout << "test2:" << i << ":" << test2[i] << endl;
-    // }
+    //! separate differnet part(chunk) to calculate
+    for (int i = 0; i < test.size() / 64; i++)
+    {
+        //? chunk
+        vector<bitset<32>> test2 = hash.chunk(vector<bitset<8>>(test.begin() + (i * 64), test.begin() + 64 * (i + 1)));
+        // for (int i = 0; i < test2.size(); i++)
+        // {
+        //     cout << "test2:" << i << ":" << test2[i] << endl;
+        // }
 
-    //! step 3
-    hash.algorithm(test2);
+        //? algorithm
+        hash.algorithm(test2);
+        // hash.state();
+    }
+
     // hash.state();
-
-    
 
     return 0;
 }
